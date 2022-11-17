27a9d6c98211d6f05af657f92b8c0d2842fb0907
your_github_username <muhammad.usama12@hotmail.com>
2022-11-17 19:18:20 +0000

    add store inventory stats to admin/dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'] , password: ENV['password']
   def show
+    @products_count = Product.count
+    @category_count = Category.count
   end
+
+
+
 end
